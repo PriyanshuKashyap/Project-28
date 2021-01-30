65404c1a027f0bf508f5d52839e868d107be2d5a
PriyanshuKashyap <74148663+PriyanshuKashyap@users.noreply.github.com>
2021-01-30 14:37:26 -0500

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -6,13 +6,14 @@ class Stone {
             friction: 1,
             density: 1.2
         };
-        this.body = Bodies.circle(-40, 370, 20, options);
+        this.body = Bodies.circle(50, 400, 20, options);
+        //-40, 370, 20, options
         this.img = loadImage("Plucking mangoes/stone.png");
         World.add(world, this.body);
     }
 
     display() {
-        image(this.img, this.body.position.x, this.body.position.y);
         //line(this.elasticConstraint.bodyB.x, this.elasticConstraint.bodyB.y, this.body.position.x, this.body.position.y);
+        image(this.img, this.body.position.x, this.body.position.y);
     }
 }
